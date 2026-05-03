3fab9111341bbe985ca0a2b4d00a4654a48a3105
JoDro79 <68988529+JoDro79@users.noreply.github.com>
2026-05-03 18:53:48 +0200

    Update schema.ts

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -3,12 +3,21 @@ import { defineSchema } from "tinacms";
 export default defineSchema({
   collections: [
     {
-      name: "content",
-      label: "Pagina's",
-      path: "content",
+      name: "gedichten",
+      label: "Gedichten",
+      path: "gedichten",
       fields: [
         { type: "string", name: "title", label: "Titel" },
-        { type: "rich-text", name: "body", label: "Inhoud" }
+        { type: "rich-text", name: "body", label: "Gedicht" }
+      ]
+    },
+    {
+      name: "verhalen",
+      label: "Verhalen",
+      path: "verhalen",
+      fields: [
+        { type: "string", name: "title", label: "Titel" },
+        { type: "rich-text", name: "body", label: "Verhaal" }
       ]
     }
   ]
